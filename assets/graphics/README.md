31cd63d9afbf2040615f55acd0aaac6b8a9b342a
Valentin Lemière <me@ibilon.com>
2014-12-06 15:09:51 +0100

    Initial commit

---
+++ assets/graphics/README.md
@@ -0,0 +1,3 @@
+# Graphic Assets
+LD31-Background.png by PlonkyDonkey
+http://ludumdare.com/compo/2014/12/03/an-ld31-background-for-all/
---
+++ src/Desktop.hx
@@ -0,0 +1,10 @@
+import com.haxepunk.Scene;
+import com.haxepunk.graphics.Image;
+
+class Desktop extends Scene
+{
+	public override function begin()
+	{
+		addGraphic(new Image("graphics/LD31-Background.png"));
+	}
+}
