547c04fe04f28b0e9a89820c755f46e6e07655c0
Valentin Lemière <me@ibilon.com>
2014-12-07 11:39:47 +0100

    Add shadow to taskbar

--- src/taskbar/TaskBar.hx
+++ src/taskbar/TaskBar.hx
@@ -10,6 +10,7 @@ class TaskBar
 	{
 		this.desktop = desktop;
 		
+		desktop.addGraphic(new Image("graphics/shadow.png"), 0, 0, 500-19);
 		desktop.addGraphic(new Image("graphics/taskbar.png"), 0, 0, 500);
 		
 		desktop.add(new Icon("start_menu", 0, 500, function () trace("cb start menu")));
